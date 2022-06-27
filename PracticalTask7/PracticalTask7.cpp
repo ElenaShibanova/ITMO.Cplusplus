bc5a28d2b9276a278122a924fabf5076c709d5b6
Елена Шибанова <elenashibanova@inbox.ru>
2022-06-28 01:00:41 +0300

    Лабораторные С++

---
+++ PracticalTask7/PracticalTask7.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+//#include <string> 
+#include "HeaderPracticalTask7.h"
+
+int main()
+{
+    system("chcp 1251");
+
+       TestDistance(); //Практика 7, упражнение 1-3
+       TestTime();//Практика 7, контрольное задание 1
+       TestMyrootStruct();//Практика 7, контрольное задание 2
+       TestMyrootTuple(); //Практика 7, контрольное задание 3
+    return 0;
+
+}
